b49da0877beeb80410b13bbcbec2531118b65009
mitchenerjosh <mitchenerjosh@gmail.com>
2016-12-02 19:38:15 -0500

    Base Jframe code

---
+++ src/com/UntitledGame/game/Main.java
@@ -0,0 +1,19 @@
+package com.UntitledGame.game;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("Ducks");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.pack();
+		frame.setSize(800, 600);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		
+	}
+
+}
