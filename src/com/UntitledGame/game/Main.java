3ebd2e0118548cf6741a6a1995e36535176cb4b7
mitchenerjosh <mitchenerjosh@gmail.com>
2016-12-02 22:45:31 -0500

    -Added folder for assets, -Added Gandalf jpg

--- src/com/UntitledGame/game/Main.java
+++ src/com/UntitledGame/game/Main.java
@@ -1,19 +1,25 @@
 package com.UntitledGame.game;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		JFrame frame = new JFrame("Ducks");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(null);
-		frame.pack();
-		frame.setSize(800, 600);
-		frame.setResizable(false);
-		frame.setVisible(true);
+		JFrame frame = new JFrame("UntitledGame");
 		
+		//frame.setSize(800, 600);
+		//frame.setResizable(false);
+		
+		ImageIcon icon = new ImageIcon("gandalf.jpg");
+		JLabel label = new JLabel(icon);
+		frame.add(label);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
