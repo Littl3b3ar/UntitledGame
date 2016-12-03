c2b114728516c46d94030111c6bf084abad1b6ab
mitchenerjosh <mitchenerjosh@gmail.com>
2016-12-02 23:04:48 -0500

    Returned to a working state

--- src/com/UntitledGame/game/Main.java
+++ src/com/UntitledGame/game/Main.java
@@ -1,24 +1,15 @@
 package com.UntitledGame.game;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		JFrame frame = new JFrame("UntitledGame");
-		
-		//frame.setSize(800, 600);
-		//frame.setResizable(false);
-		
-		ImageIcon icon = new ImageIcon("gandalf.jpg");
-		JLabel label = new JLabel(icon);
-		frame.add(label);
-		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
+		frame.setSize(800, 600);
+		frame.setResizable(false);
 		frame.setVisible(true);
 	}
 
