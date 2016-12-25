f194f0ee3bb7f71f2b46bff1ca7871ee115da32d
mitchenerjosh <mitchenerjosh@gmail.com>
2016-12-24 21:40:56 -0500

    added player class, simmilar to entity

--- src/com/untitledgame/game/Player.java
+++ src/com/untitledgame/game/Player.java
@@ -1,5 +1,85 @@
 package com.untitledgame.game;
 
 public class Player {
+	
+	//Player visibility to the renderer
+	private boolean playerVisible = false;
+	
+	// Coordinates of the player
+	private int playerXLoc, playerYLoc, playerZPri;
+	private int playerLength, playerHeight;
+	private String playerFilePath;
+	
+	public Player (int x,int y,int zPriority){
+		
+	}
+	
+	public void setPlayerVisibility(boolean playerVisibility){
+		this.playerVisible = playerVisibility;
+	}
+	
+	public boolean getPlayerVisibility(){
+		return this.playerVisible;
+	}
+	
+	public int getPlayerXLoc( )
+	{
+		return this.playerXLoc;
+	}
+	
+	public int getPlayerYLoc( )
+	{
+		return this.playerYLoc;
+	}
+	
+	public int getPlayerZPri( )
+	{
+		return this.playerZPri;
+	}
+	
+	public int getPlayerLength( )
+	{
+		return this.playerLength;
+	}
+	
+	public int getPlayerHeight( )
+	{
+		return this.playerHeight;
+	}
+	
+	public String getPlayerFilePath( )
+	{
+		return this.playerFilePath;
+	}
+	
+	public void setPlayerXLoc(int newPlayerXLoc)
+	{
+		this.playerXLoc = newPlayerXLoc;
+	}
+	
+	public void setPlayerYLoc(int newPlayerYLoc)
+	{
+		this.playerYLoc = newPlayerYLoc;
+	}
+	
+	public void setPlayerZPri(int newPlayerZPri)
+	{
+		this.playerZPri = newPlayerZPri;
+	}
+	
+	public void setPlayerLength(int newPlayerLength)
+	{
+		this.playerLength = newPlayerLength;
+	}
+	
+	public void setPlayerHeight(int newPlayerHeight)
+	{
+		this.playerHeight = newPlayerHeight;
+	}
+	
+	public void setPlayerFilePath(String newPlayerFilePath)
+	{
+		this.playerFilePath = newPlayerFilePath;
+	}
 
 }
